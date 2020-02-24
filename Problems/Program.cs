bbb37ae91e11c726a40de80d6b6ab8f1e90e682d
Kushinga <kushingapfende@gmail.com>
2020-02-24 22:20:38 +0200

    Just added Program.cs

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,43 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input= Console.ReadLine();
+            Console.WriteLine(Prefix(input));
+        }
+
+        public static string Prefix(string input)
+        {
+            string myResult="";
+            int words=1;
+
+                 if(input=="")
+                {
+                    words=0;
+                }
+
+            for(int i=0; i<input.Length; i++)
+            {
+                if(input!=null)
+                {                   
+                    if(input[i]==' ' && input[i+1]!=' ')
+                    {
+
+                        words++;
+                    }
+                }
+                else
+                {
+                    return "null";
+                }
+
+            }
+            myResult=$"{input.Length}, {words}, {input}";
+
+            return myResult;
         }
     }
 }
